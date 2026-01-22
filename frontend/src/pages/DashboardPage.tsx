86c7d390a17e881dbcb8d87aa4373b4187b96a2b
Flynnpen <flp203@exeter.ac.uk>
2026-01-22 13:15:29 +0000

    Set up Tailwind CSS and start dashboard UI

---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Dashboard</h1>
+      <p>If you can see this, your React app is working.</p>
+    </div>
+  );
+}
